f2199cd0f10d55e47105b7d1ffca1a5ccc8faa15
Doppel_R <doppelr1001@gmail.com>
2025-09-25 19:25:07 +0200

    attempt to fix being unable to merge PR

--- src/main/java/net/doppelr/lemonmates/Config.java
+++
@@ -1,7 +0,0 @@
-package net.doppelr.lemonmates;
-
-// An example config class. This is not required, but it's a good idea to have one to keep your config organized.
-// Demonstrates how to use Neo's config APIs
-public class Config {
-
-}
